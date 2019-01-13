222a4be30b8874cf34c1be3f710628d2597499fa
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-01-13 20:32:18 -0200

    Add files via upload

--- angular-med-store/src/app/app-routing.module.ts
+++ angular-med-store/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarrinhoPageComponent } from './viewmodel/carrinho-page/carrinho-page.component';
+import { CompraComponent } from './view/compra/compra.component';
+import {HomeComponent } from './view/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'cart',
+    component: CarrinhoPageComponent,
+    children: [],
+  },
+  {
+    path: 'compra',
+    component: CompraComponent,
+    children: [],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-med-store/src/app/app.module.ts
+++ angular-med-store/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material-module/material-module.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './view/home/home.component';
+import { CarrinhoIndicadorComponent } from './viewmodel/carrinho-indicador/carrinho-indicador.component';
+import { ModelModule } from './model';
+import { CarrinhoPageComponent } from './viewmodel/carrinho-page/carrinho-page.component';
+import { CarrosselProdutosComponent } from './view/carrossel-produtos/carrossel-produtos.component';
+import { ItemProdutoGradeComponent } from './view/item-produto-grade/item-produto-grade.component';
+import { CompraComponent } from './view/compra/compra.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarrinhoIndicadorComponent,
+    CarrinhoPageComponent,
+    CarrosselProdutosComponent,
+    ItemProdutoGradeComponent,
+    CompraComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModelModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
