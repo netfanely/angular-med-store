0e750857e3cdbe24be3ae8a9e1961557c5feb424
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-01-17 10:50:10 -0200

    Add files via upload

---
+++ angular-med-store/src/app/viewmodel/compra/compra.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-compra',
+  templateUrl: './compra.component.html',
+  styleUrls: ['./compra.component.scss']
+})
+export class CompraComponent implements OnInit {
+
+  constructor(
+    ){
+   }
+
+  ngOnInit() {
+  }
+
+}
